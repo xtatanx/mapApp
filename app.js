4f85892277d0a00ac9b4635c351a5989f1b23a60
unknown <jhonnatanhxc@gmail.com>
2014-03-18 15:36:22 -0500

    when users connect to website appear show a marker in random place

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.createServer(app);
+var http = require('http');
+var server = http.createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 
 
 app.configure(function(){
 	app.use('/', express.static(__dirname + '/public'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-
+/* socket function */
 io.sockets.on('connection', function(socket){
-	socket.emit('news', {hello: 'world'});
-	socket.on('my other event', function(data){
-		console.log(data);
+	/* every time someone connects place a marker in the map */
+	socket.on('send:coords', function(data){
+		socket.broadcast.emit('load:coords', data);
 	});
+
 });
 
-server.listen(3000, 'localhost');
+
 
 
 // console.log('Listening on port %d', server.address().port);
