051f85368fd9bf1b5b84c029bdc793d8956e6714
unknown <jhonnatanhxc@gmail.com>
2014-03-22 16:50:56 -0500

    renders people actually conected - data senden by the server

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+	// Neccesary vars
+	var id = Math.random().toString(36).substr(2,16);
+	var sendData = {
+		id:0,
+		lat:0,
+		lng:0
+	}
+
 	// create a map in the "map" div, set the view to a given place and zoom
 	var map = L.map('map',{
 		center:[0,0],
@@ -20,12 +28,6 @@ $(function(){
 	map.locate({setView:true, maxZoom:16, enableHighgAccuracy:true});
 	map.on('locationfound', onLocationFound);
 
-
-
-	var coords = coords || {};
-	/* if The client support sockets create a connection */
-
-
 	// helper function for creating markers
 	function createMarker(lat, lng){
 		var marker = L.marker([lat,lng]).addTo(map);
@@ -33,24 +35,38 @@ $(function(){
 
 	// function to execute after get location
 	function onLocationFound(e){
-		coords.lat = e.latitude;
-		coords.lng = e.longitude;
-		createMarker(coords.lat, coords.lng);
+		// sendData.lat = e.latitude;
+		// sendData.lng = e.longitude;
+
+		// random numbers local testing
+		sendData.lat = Math.floor((Math.random()* 50) +1);
+		sendData.lng = Math.floor((Math.random()* 50) +1);		
+		sendData.id = id;
+		console.log(sendData);
+		// createMarker(sendData.lat, sendData.lng);
 		createConnection();
+	}
+
+	/* set  initial markers markers*/
+	function setMarkers(connections){
+		for(var i = 0; i < connections.length; i++){
+			createMarker(connections[i].lat, connections[i].lng);
+		}
 	}
 
 	/* function to connect the socket*/
 	function createConnection(){
+		/* if The client support sockets create a connection */
 		if(Modernizr.websockets){
 			/* client socket */
 			var host = location.origin.replace(/^http/, 'ws');
 			var socket = io.connect(host);
 
-			socket.emit('send:coords', coords);
+			socket.emit('send:coords', sendData);
 
 			socket.on('load:coords', function(data){
-				console.log(data);
-				createMarker(data.lat, data.lng);
+				console.log("aloooooo");
+				setMarkers(data);
 			});
 		}
 	}	
