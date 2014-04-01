8d23666cb9012d371789c5d3649440d821c2bf66
unknown <jhonnatanhxc@gmail.com>
2014-04-01 16:30:42 -0500

    server doesnt store connections anymore, send id of socket disconnected

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var port = Number(process.env.PORT || 5000)
 var server = http.createServer(app).listen(port);
 var io = require('socket.io').listen(server);
 
-var connections = [];
-
 app.configure(function(){
 	app.use('/', express.static(__dirname + '/public'));
 });
@@ -17,36 +15,17 @@ app.configure('development', function(){
 
 /* socket function */
 io.sockets.on('connection', function(socket){
-	/* every time server reciebe data emit to all  */
+	/* every time server recieve data emit to all  */
 	socket.on('send:coords', function(data){
-		// if id doesnt exist in connections push it
-		if(!checkAvalibility(data.id)){
-			connections.push(data);
-		}
-
-		io.sockets.emit('load:coords', connections);
+		data.id = socket.id;
+		socket.broadcast.emit('load:coords', data);
 	});
 
 	socket.on('disconnect', function(){
-		io.sockets.emit('user:disconnected');
+		socket.broadcast.emit('user:disconnected', socket.id);
 	});	
 
 });
-
-
-// helper function to check if id exist in array
-function checkAvalibility(id){
-	for (var i = 0; i < connections.length; i++){
-			if(connections[i].id === id){
-				return true;
-			}
-	}
-	return false;
-}
-
-
-
-
 
 // console.log('Listening on port %d', server.address().port);
 
